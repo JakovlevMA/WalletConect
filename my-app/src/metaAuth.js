c9ff6dd04a1144a799955f346a27cba27f164d7f
KRYPTOKRASH <maxbaga_1506@mail.ru>
2022-09-01 14:55:05 +0400

    add alert

--- my-app/src/metaAuth.js
+++ my-app/src/metaAuth.js
@@ -1,1 +1,20 @@
 import { ethers } from ethers; 
+import React, { useEffect, useState } from "react";
+
+
+function isMobileDevice() {
+    return 'ontouchstart' in window || 'onmsgesturechange' in window;
+  }
+  
+  async function connect(onConnected) {
+    if (!window.ethereum) {
+      alert("Get MetaMask!");
+      return;
+    }
+  
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+  
+    onConnected(accounts[0]);
+  }
