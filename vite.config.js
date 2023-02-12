d78838db6c79618b36312b7669fe0120ea3b2dee
JakovlevMA <125180332+JakovlevMA@users.noreply.github.com>
2023-02-12 17:21:04 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-// попытка изменений//
+/// попытка изменений///
 export default defineConfig({
   plugins: [react()],
   server: {
