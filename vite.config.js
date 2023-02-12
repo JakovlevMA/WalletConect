2aa79a8e7049f391756404d0c628434d9bb28bf8
misha <lolipopp345@gmail.com>
2023-02-12 16:43:43 +0300

    коммент прописан

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+/// новые изменения))))
 export default defineConfig({
   plugins: [react()],
   server: {
